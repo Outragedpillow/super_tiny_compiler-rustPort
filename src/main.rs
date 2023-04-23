e05ccce6f90793e4d2ed946ab82e2aba17078b38
PThorpe92 <preston.thorpe@maine.edu>
2023-04-22 21:52:34 -0400

    fn as hashmap val..

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 // Port of https://github.com/hazbo/the-super-tiny-compiler, which gets all credit for all
 //explanations, comments, etc. Still work in progress
+use std::collections::HashMap;
+use std::error::Error;
 
 #[derive(Debug, PartialEq)]
 pub struct Token {
@@ -147,31 +149,20 @@ fn parser<'a>(tokens: Vec<&Token>, &mut pc: usize, &mut pt: Vec<&Token>) -> Ast<
         arguments: None,
         context: None,
     };
-   while pc < len(pt) {
+   while pc < pt.len() {
       ast.body.push(walk(pc, pt));
    }
     return ast;
 }
 // Instead of a 'while' loop to access our values, this function will do it recursively
-fn walk(&mut pc: usize, &mut pt: Vec<&Token>) -> Node<'static> {
+fn walk(&mut pc: usize, &mut pt: Vec<&Token>) -> Result<Node<'static>, Error> {
     // Inside the walk funciton we start by grabbing the 'current' token
-   let token = pt[pc];
+    let token = pt[pc];
 
     // We can split each type of token off into a diff code path,
     // beginning with 'number' tokens.
     if token.parse::<f64>().is_ok() {
-       pc += 1;
-        return Node {
-            kind: Option::from(String::from("NumberLiteral")),
-            value: Option::from(token.value),
-            name: None,
-            callee: None,
-            expression: None,
-            body: None,
-            params: None,
-            arguments: None,
-            context: None,
-        };
+        pc += 1;
     }
     // Next we look for CallExpressions. (open parenthesis)
     if token.kind == "paren" && token.value == "(" {
@@ -196,51 +187,98 @@ fn walk(&mut pc: usize, &mut pt: Vec<&Token>) -> Node<'static> {
         };
         pc += 1;
         token = pt[pc];
-    }
-		// And now we want to loop through each token that will be the `params` of
-		// our `CallExpression` until we encounter a closing parenthesis.
-		//
-		// Now this is where recursion comes in. Instead of trying to parse a
-		// potentially infinitely nested set of nodes we're going to rely on
-		// recursion to resolve things.
-		//
-		// To explain this, let's take our Lisp code. You can see that the
-		// parameters of the `add` are a number and a nested `CallExpression` that
-		// includes its own numbers.
-		//
-		//   (add 2 (subtract 4 2))
-		//
-		// You'll also notice that in our tokens array we have multiple closing
-		// parenthesis.
-		//
-		//   [
-		//     { type: 'paren',  value: '('        },
-		//     { type: 'name',   value: 'add'      },
-		//     { type: 'number', value: '2'        },
-    	//     { type: 'paren',  value: '('        },
-		//     { type: 'name',   value: 'subtract' },
-		//     { type: 'number', value: '4'        },
-		//     { type: 'number', value: '2'        },
-		//     { type: 'paren',  value: ')'        }, <<< Closing parenthesis
-		//     { type: 'paren',  value: ')'        }  <<< Closing parenthesis
-		//   ]
-		//
-		// We're going to rely on the nested `walk` function to increment our
-		// `current` variable past any nested `CallExpressions`.
+        // And now we want to loop through each token that will be the `params` of
+        // our `CallExpression` until we encounter a closing parenthesis.
+        //
+        // Now this is where recursion comes in. Instead of trying to parse a
+        // potentially infinitely nested set of nodes we're going to rely on
+        // recursion to resolve things.
+        //
+        // To explain this, let's take our Lisp code. You can see that the
+        // parameters of the `add` are a number and a nested `CallExpression` that
+        // includes its own numbers.
+        //
+        //   (add 2 (subtract 4 2))
+        //
+        // You'll also notice that in our tokens array we have multiple closing
+        // parenthesis.
+        //
+        //   [
+        //     { type: 'paren',  value: '('        },
+        //     { type: 'name',   value: 'add'      },
+        //     { type: 'number', value: '2'        },
+        //     { type: 'paren',  value: '('        },
+        //     { type: 'name',   value: 'subtract' },
+        //     { type: 'number', value: '4'        },
+        //     { type: 'number', value: '2'        },
+        //     { type: 'paren',  value: ')'        }, <<< Closing parenthesis
+        //     { type: 'paren',  value: ')'        }  <<< Closing parenthesis
+        //   ]
+        //
+        // We're going to rely on the nested `walk` function to increment our
+        // `current` variable past any nested `CallExpressions`.
 
-		// So we create a `while` loop that will continue until it encounters a
-		// token with a `type` of `'paren'` and a `value` of a closing
-		// parenthesis.
-   while token.kind != "paren"  || (token.kind == "paren" && token.value != ")" {
-       // we call the 'walk' function which will return a 'node' and we'll push it into out 'node.params'.
-       n.params.push(walk(pc, pt));
-       token = pt[pc];
-   }
-    // Finally we will increment 'current' one last time to skip the closing
-    //parenthesis
-    pc += 1;
-    return n;
+        // So we create a `while` loop that will continue until it encounters a
+        // token with a `type` of `'paren'` and a `value` of a closing
+        // parenthesis.
+        while token.kind != "paren" || (token.kind == "paren" && token.value != ")") {
+            // we call the 'walk' function which will return a 'node' and we'll push it into out 'node.params'.
+            n.params.push(walk(pc, pt));
+            token = pt[pc];
+        }
+        // Finally we will increment 'current' one last time to skip the closing
+        //parenthesis
+        pc += 1;
+        return Ok(n);
+    }
+
+    let result = Node { kind: Option::from(String::from("NumberLiteral")), value: Option::from(token.value), name: None, callee: None, expression: None, body: None, params: None, arguments: None, context: None, };
+    return match result {
+        Ok(result) => result,
+        Err(e) => panic!("{}", e)
+    }
 }
+// The visitor will consist of a string and a function associated with it.
+// For each instance variable we defined earlier, we will be able to access a function
+// via the value of the string stored as a key in the visitor hashmap
+type Visitor<'a> = HashMap<&'a str, fn(&Node<'a>, Node<'a>)>;
+
+fn traverser(a: Ast, v: Visitor) {
+
+    // We call "traverseNode" with our ast with no "parent" because the top level of the
+    // AST doesn't have one
+    traverseNode(node(a), p, v);
+}
+// a traverseArray function, that will allow us to iterate over a slice and
+// call the next function that we will define: "traverseNode
+fn traverseArray(a: Vec<Node>, p: Node, v: Visitor) {
+    for child in a {
+       traverseNode(child, p, v);
+    }
+}
+fn traverseNode(n: Node, p: Node, v: Visitor) {
+    // we iterate over the visitor we pass to the traverseNode function, and the value
+    // will be the function. We call it with the node and its parent
+    for (key, value) in &v {
+        if k == n.kind {
+        value(&n, *p)
+    }
+    }
+    // Next we split things up by the current node type
+    match n.kind {
+
+        // We start at top level "Program". Since program nodes have a prop named
+        // 'body' that has a Vector of nodes, we will call 'traverseArray" to traverse
+        // down into them. traverseArray will in turn call "traverseNode" so we are
+        // causing the tree to traversed recursively
+        String::from("Program") => traverseArray(n.body, n, v),
+
+
+    }
+}
+
+
+
 
  fn main() {
     println!("way more complicated than the Go port....")
