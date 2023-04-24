8994f1080b61cfcd2e860c787349f238272f5d7f
PThorpe92 <preston.thorpe@maine.edu>
2023-04-23 21:32:09 -0400

    remove target file/.gitignore

--- target/debug/deps/tiny_compiler-3346dfb1d12ae827.d
+++
@@ -1,5 +0,0 @@
-/home/dvektor/Documents/super_tiny_compiler-rustPort/target/debug/deps/tiny_compiler-3346dfb1d12ae827.rmeta: src/main.rs
-
-/home/dvektor/Documents/super_tiny_compiler-rustPort/target/debug/deps/tiny_compiler-3346dfb1d12ae827.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/tiny_compiler-35d1b85512629c69.d
+++
@@ -1,5 +0,0 @@
-/home/dvektor/Documents/super_tiny_compiler-rustPort/target/debug/deps/tiny_compiler-35d1b85512629c69: src/main.rs
-
-/home/dvektor/Documents/super_tiny_compiler-rustPort/target/debug/deps/tiny_compiler-35d1b85512629c69.d: src/main.rs
-
-src/main.rs:
